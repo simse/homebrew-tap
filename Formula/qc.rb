ab1d3b8c00d7b00ea24f0de448da9417d948af3d
Simon Sørensen <6843486+simse@users.noreply.github.com>
2021-08-22 11:07:43 +0200

    Update qc to v0.8.5

--- Formula/qc.rb
+++ Formula/qc.rb
@@ -1,26 +1,26 @@
 class QuickConvert < Formula
-    desc "qc is a tool for converting between file formats"
-    homepage "https://qc.simse.io"
-    version "v0.8.3"
-    url "https://github.com/simse/qc.git", tag: "v0.8.3", revision: "bdfeeb390f2cca604308d1fed8f2644064f0f4ec"
-  
-    depends_on "go" => :build
-    depends_on "vips" => :build
-    depends_on "pkg-config" => :build
-    depends_on "vips"
-  
-    def install
-      install
-      ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
-      ENV["CGO_ENABLED"] = "1"
-      ENV["CGO_CFLAGS_ALLOW"] = "-Xpreprocessor"
-      
-      system "go", "run", "build.go", "--enable-cgo"
-  
-      bin.install "qc"
-    end
-  
-    test do
-      system "false"
-    end
+  desc "qc is a tool for converting between file formats"
+  homepage "https://qc.simse.io"
+  version "v0.8.5"
+  url "https://github.com/simse/qc.git", tag: "v0.8.5", revision: "08eac98cda4b2301662be646c6684782a09c10fc"
+
+  depends_on "go" => :build
+  depends_on "vips" => :build
+  depends_on "pkg-config" => :build
+  depends_on "vips"
+
+  def install
+    install
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+    ENV["CGO_ENABLED"] = "1"
+    ENV["CGO_CFLAGS_ALLOW"] = "-Xpreprocessor"
+
+    system "go", "run", "build.go", "--enable-cgo"
+
+    bin.install "qc"
   end
+
+  test do
+    system "false"
+  end
+end
