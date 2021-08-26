d6be42d08a886d08bf37acd8fc2e5f02e1e123bb
Simon Sørensen <hello@simse.io>
2021-08-26 18:32:50 +0200

    fix qc formula

--- Formula/qc.rb
+++ Formula/qc.rb
@@ -1,7 +1,8 @@
 class Qc < Formula
   desc "Tool for converting between file formats"
   homepage "https://qc.simse.io"
-  url "https://github.com/simse/qc.git", tag: "0.8.5", revision: "08eac98cda4b2301662be646c6684782a09c10fc"
+  version "0.8.5"
+  url "https://github.com/simse/qc.git", tag: "v0.8.5", revision: "08eac98cda4b2301662be646c6684782a09c10fc"
 
   depends_on "go" => :build
   depends_on "pkg-config" => :build
