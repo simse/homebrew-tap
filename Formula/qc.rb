4a2ff7df9ed8e627df18d923986fdb16819d07ad
Simon Sørensen <hello@simse.io>
2021-08-26 13:25:11 +0200

    added checksum

--- Formula/qc.rb
+++ Formula/qc.rb
@@ -2,6 +2,7 @@ class Qc < Formula
   desc "Tool for converting between file formats"
   homepage "https://qc.simse.io"
   url "https://github.com/simse/qc/archive/refs/tags/v0.8.5.tar.gz"
+  sha256 "611906e56c4398b0878da12d81f42d0b7607aae253b28e904e7cbbff627034b2"
 
   depends_on "go" => :build
   depends_on "pkg-config" => :build
