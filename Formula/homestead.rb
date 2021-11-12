239757e1a04bb1dd9e1e9841440f8faba4f5f581
Simon Sørensen <6843486+simse@users.noreply.github.com>
2021-11-12 16:37:25 +0000

    Update homestead to 0.0.3

--- Formula/homestead.rb
+++ Formula/homestead.rb
@@ -1,8 +1,8 @@
 class Homestead < Formula
   desc "Tool for creating local, isolated development environments"
   homepage "https://homestead.simse.io"
-  version "0.0.1"
-  url "https://github.com/simse/homestead.git", tag: "0.0.1", revision: "d99305c7eb721994edcbc57d3f01abe99a0f0f9a"
+  version "0.0.3"
+  url "https://github.com/simse/homestead.git", tag: "0.0.3", revision: "9062ee93f92948e5eb5dc660c34e2b3b051fc866"
 
   depends_on "go" => :build
 
